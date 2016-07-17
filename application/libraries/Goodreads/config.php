6e467ea3782d1b95cde40776b71fd92bfa013aa4
hani <hani@INFO-DXB-LP0011.infotouchme.com>
2016-07-17 22:07:55 +0400

    checked in bookmatch me

---
+++ application/libraries/Goodreads/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * you will get the following details after
+ * registering your application at
+ * http://www.goodreads.com/api/keys
+ *
+ */
+
+
+define('CONSUMER_KEY', '4WGPtxFx1tlhlQyGSO96A');
+define('CONSUMER_SECRET', 'aNi8HnHuZvDaNcRgzDpqmMUjXsI7RGruSwsXW8SU');
+define('CALLBACK_URL', 'http://localhost/goodreadstestproject/callback.php');
+
+
+?>
